17488877f7083d0666b14478382d164e249035fe
Saymon <Saymon@192.168.1.100>
2014-04-04 11:32:02 -0300

    Capítulo 11	 Conteúdo da API: Datas, Internacionalização, Formatação com DateFormat Capítulo 12	 Conteúdo da API: Formatação com NumberFormat e printf

--- src/br/certificacao/ocjp6/conteudoAPI/Strings.java
+++ src/br/certificacao/ocjp6/conteudoAPI/Strings.java
@@ -2,8 +2,8 @@ package br.certificacao.ocjp6.conteudoAPI;
 
 /**
  * <h1>Strings</h1><br/>
- * Todas possuem o metodo length() que retorna o numero de caracteres e imlementam a interface {@link CharSequence}
- * <br/>
+ * Todas possuem o metodo length() que retorna o numero de caracteres e
+ * imlementam a interface {@link CharSequence} <br/>
  * <h2>{@link String}</h2><br/>
  * São Objetos imutáveis onde uma vez construido não pode ter seu valor
  * alterado;<br/>
@@ -31,17 +31,21 @@ package br.certificacao.ocjp6.conteudoAPI;
  * 
  * <br/>
  * <br/>
- * <h2>{@link StringBuffer} e {@link StringBuilder} </h2><br/>
+ * <h2>{@link StringBuffer} e {@link StringBuilder}</h2><br/>
  * São Mutáveis seu valor pode ser alterado.<br/>
- * {@link StringBuffer} Possui seus metodos como synchronized, portanto pode ser utilizado em concorrencia de multiplas {@link Thread}<br/>
- * {@link StringBuilder} Possui os mesmos metodos do {@link StringBuffer} porém sem o modificador synchronized, tem o melhor desempenho entre {@link String} e {@link StringBuilder};<br/>    
+ * {@link StringBuffer} Possui seus metodos como synchronized, portanto pode ser
+ * utilizado em concorrencia de multiplas {@link Thread}<br/>
+ * {@link StringBuilder} Possui os mesmos metodos do {@link StringBuffer} porém
+ * sem o modificador synchronized, tem o melhor desempenho entre {@link String}
+ * e {@link StringBuilder};<br/>
  * 
- * <h3>Principais metodos :</h3>
- * <h4>apend(String s ou primitivo p)</h4>:Acrescenta caracteres no final.
- * <h4>delete(int inicio ,int fim)</h4>:Remove caracteres entre as posições inicio e fim -1.
- * <h4>reverse()</h4>:Inverte o conteudo da String,Ex:"SAYMON" para "NOMYAS".
- * <h4>insert(int inicio,String s ou primitivo p)</h4>:Insere entre a posição de inicio e realoca os caracteres a direita;
- * <h4>toString()</h4>:Retorna uma String com o Conteudo encapsulado ;
+ * <h3>Principais metodos :</h3> <h4>apend(String s ou primitivo p)</h4>
+ * :Acrescenta caracteres no final. <h4>delete(int inicio ,int fim)</h4>:Remove
+ * caracteres entre as posições inicio e fim -1. <h4>reverse()</h4>:Inverte o
+ * conteudo da String,Ex:"SAYMON" para "NOMYAS". <h4>insert(int inicio,String s
+ * ou primitivo p)</h4>:Insere entre a posição de inicio e realoca os caracteres
+ * a direita; <h4>toString()</h4>:Retorna uma String com o Conteudo encapsulado
+ * ;
  * 
  * @author Saymon
  * 
@@ -51,18 +55,19 @@ public class Strings {
 
 	/**
 	 * <h1>Exemplo Pool de Strings</h1><br/>
-	 * String s1 = " abc " + " def ";<br/>
-	 * String s2 = " abc " + " def ";<br/>
-	 * System.out.println(("s1 == s2 ") + (s1 == s2 ? "São Iguais" :
-	 * "São diferentes"));<br/>
-	 * s2 = new String(s1);<br/>
-	 * System.out.println(("s1 == s2 ") + (s1 == s2 ? "São Iguais" :
-	 * "São diferentes"));<br/>
-	 * System.out.println(("s1.equals(s2) ") + (s1.equals(s2) ? "São Iguais" :
-	 * "São diferentes"));<br/>
-	 * <br/>
-	 * O codigo acima apresenta o seguinte resultado : <br/><b>
-	 * s1 == s2 São Iguais<br/>
+	 * <blockquote>
+	 * 
+	 * <pre>
+	 * String s1 = " abc " + " def ";
+	 * String s2 = " abc " + " def ";
+	 * System.out.println(("s1 == s2 ") + (s1 == s2 ? "São Iguais" : "São diferentes"));
+	 * s2 = new String(s1);
+	 * System.out.println(("s1 == s2 ") + (s1 == s2 ? "São Iguais" : "São diferentes"));
+	 * System.out.println(("s1.equals(s2) ")+ (s1.equals(s2) ? "São Iguais" : "São diferentes"));
+	 * </pre>
+	 * </blockquote> 
+	 * O codigo acima apresenta o seguinte resultado : <br/>
+	 * <b> s1 == s2 São Iguais<br/>
 	 * s1 == s2 São diferentes<br/>
 	 * s1.equals(s2) São Iguais<br/>
 	 * </b>
@@ -79,12 +84,12 @@ public class Strings {
 				+ (s1.equals(s2) ? "São Iguais" : "São diferentes"));
 
 	}
+
 	/**
 	 * <h1>Exemplo String Builder</h1><br/>
 	 * StringBuilder builder = new StringBuilder("asdfg");<br/>
 	 * builder.insert(3, "qwert").reverse().append(123).delete(6, 9);<br/>
-	 * <b>
-	 * Teste de Mesa abaixo:<br/>
+	 * <b> Teste de Mesa abaixo:<br/>
 	 * -> builder é igual á "asdfg"<br/>
 	 * builder.insert(3, "qwert")<br/>
 	 * -> builder passa a ser igual á "asdqwertfg"<br/>
@@ -94,8 +99,7 @@ public class Strings {
 	 * -> builder passa a ser igual á "gftrewqdsa123"<br/>
 	 * builder.delete(6, 9)<br/>
 	 * -> builder passa a ser igual á "gftrewa123"<br/>
-	 * </b>
-	 * System.out.println(builder);<br/>
+	 * </b> System.out.println(builder);<br/>
 	 * Resultado impresso é "gftrewa123"<br/>
 	 */
 	public void exemploStringBuilder() {
--- src/br/certificacao/ocjp6/declaracaoInicializacaoEscopo/PadroesJavaBeans.java
+++ src/br/certificacao/ocjp6/declaracaoInicializacaoEscopo/PadroesJavaBeans.java
@@ -1,6 +1,5 @@
 package br.certificacao.ocjp6.declaracaoInicializacaoEscopo;
 
-import java.util.EventObject;
 
 /**
  * 
@@ -9,11 +8,11 @@ import java.util.EventObject;
  * Não é requerido pelo compilador,mas é uma boa pratica de programação utilizada para o JAVA.
  * <br/>
  * <h3>Conceito de Propriedade :</h3><br/> 
- * Toda variavel de instancia deve ser de preferencia declarada como private e manipulada por metodos de acesso <b>set</b> para atribuição e <b>get</b> para leitura,
+ * Toda variável de instância deve ser de preferência declarada como private e manipulada por métodos de acesso <b>set</b> para atribuição e <b>get</b> para leitura,
  * seguidos do nome do atributo.Ex : setNomeAtributo(),getNomeAtributo();<br/>
  * Nos casos de tipo boolean aconselha-se is no lugar de get.
  * <br/>
- * Variaveis e metodos devem ser definidas utilizando na primeira letra minuscula e para as proximas palavras a primeiraletra maiuscula.<br/>
+ * Variáveis e métodos devem ser definidas utilizando na primeira letra minúscula e para as próximas palavras a primeira letra maiúscula.<br/>
  * <br/>
  * int variavelInteira;<br/>
  * ou <br/>
